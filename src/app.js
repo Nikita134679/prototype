c350dc5851ad1510eb5979403469a63d1438018f
Nikita Soloviev <Nikita134679@gmail.com>
2024-05-17 02:58:04 +0300

    second commit

--- src/app.js
+++ src/app.js
@@ -1,58 +1,77 @@
-class Character {
+export class Character {
   constructor(name, type) {
     if (name.length < 2) {
       throw new Error('Name is too short');
-    }
+    };
     if (name.length > 10) {
       throw new Error('Name is too long');
-    }
+    };
     if (type !== 'Bowman' && type !== 'Swordsman' && type !== 'Magician' && type !== 'Daemon' && type !== 'Undead' && type !== 'Zombie') {
       throw new Error('Type is not correct');
-    }
+    };
     this.name = name;
     this.type = type;
     this.health = 100;
     this.level = 1;
     this.attack = 0;
     this.defence = 0;
-    if (this.type === 'Bowman') {
-      this.attack = 25;
-      this.defence = 25;
+
+    function levelUp() {
+      if (this.health > 0) {
+        this.level += 1;
+        this.attack += this.attack * 0.2;
+        this.defence += this.defence * 0.2;
+        this.health = 100;
+      };
     };
-    if (this.type === 'Swordsman') {
-      this.attack = 40;
-      this.defence = 40;
-    };
-    if (this.type === 'Magician') {
-      this.attack = 10;
-      this.defence = 40;
-    };
-    if (this.type === 'Undead') {
-      this.attack = 25;
-      this.defence = 25;
-    };
-    if (this.type === 'Zombie') {
-      this.attack = 40;
-      this.defence = 40;
-    };
-    if (this.type === 'Daemon') {
-      this.attack = 10;
-      this.defence = 40;
+    function damage(points) {
+      if (this.health >= 0) {
+        this.health -= points * (1 - this.defence / 100);
+      } else {
+        throw new Error('Character is dead');
+      };
     };
   };
-  function levelUp() {
-    if (this.health > 0) {
-      this.level += 1;
-      this.attack += this.attack * 0.2;
-      this.defence += this.defence * 0.2;
-      this.health = 100;
-    }
+};
+export class Bowman extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 25;
+    this.defence = 25;
   };
-  function damage(points) {
-    if (this.health >= 0) {
-      this.health -= points * (1 - this.defence / 100);
-    } else {
-      throw new Error('Character is dead');
-    }
+};
+export class Swordsman extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 40;
+    this.defence = 10;
+  };
+};
+export class Magician extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 10;
+    this.defence = 40;
+  };
+};
+export class Daemon extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 10;
+    this.defence = 40;
+  };
+};
+export class Undead extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 25;
+    this.defence = 25;
+  };
+};
+export class Zombie extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 40;
+    this.defence = 10;
   };
 };
---
+++ test/test.js
@@ -0,0 +1,140 @@
+import { Character } from '../src/app.js';
+import { Bowman } from '../src/app.js';
+import { Swordsman } from '../src/app.js';
+import { Magician } from '../src/app.js';
+import { Daemon } from '../src/app.js';
+import { Undead } from '../src/app.js';
+import { Zombie } from '../src/app.js';
+
+test.each([
+    ['Bowman', 'Bowman', {
+        name: 'Bowman',
+        type: 'Bowman',
+        health: 100,
+        level: 1,
+        attack: 0,
+        defence: 0
+      }],
+    ['Swordsman', 'Swordsman', {
+        name: 'Swordsman',
+        type: 'Swordsman',
+        health: 100,
+        level: 1,
+        attack: 0,
+        defence: 0
+      }],
+    ['Magician', 'Magician', {
+        name: 'Magician',
+        type: 'Magician',
+        health: 100,
+        level: 1,
+        attack: 0,
+        defence: 0
+      }],
+    ['Daemon', 'Daemon', {
+        name: 'Daemon',
+        type: 'Daemon',
+        health: 100,
+        level: 1,
+        attack: 0,
+        defence: 0
+      }],
+    ['Undead', 'Undead', {
+        name: 'Undead',
+        type: 'Undead',
+        health: 100,
+        level: 1,
+        attack: 0,
+        defence: 0
+      }],
+    ['Zombie', 'Zombie', {
+        name: 'Zombie',
+        type: 'Zombie',
+        health: 100,
+        level: 1,
+        attack: 0,
+        defence: 0
+      }],
+]) (
+('test characted'), (name, type, expected) => {
+    const result = new Character(name,type);
+    expect(result).toEqual(expected);
+});
+
+test('test character Bowman', () => {
+    const received = new Bowman('Bowman', 'Bowman')
+    const expected = {
+        name: 'Bowman',
+        type: 'Bowman',
+        health: 100,
+        level: 1,
+        attack: 25,
+        defence: 25
+    }
+    expect(received).toEqual(expected);
+});
+
+test('test character Swordsman', () => {
+    const received = new Swordsman('Swordsman', 'Swordsman')
+    const expected = {
+        name: 'Swordsman',
+        type: 'Swordsman',
+        health: 100,
+        level: 1,
+        attack: 40,
+        defence: 10
+    }
+    expect(received).toEqual(expected);
+});
+
+test('test character Magician', () => {
+    const received = new Magician('Magician', 'Magician')
+    const expected = {
+        name: 'Magician',
+        type: 'Magician',
+        health: 100,
+        level: 1,
+        attack: 10,
+        defence: 40
+    }
+    expect(received).toEqual(expected);
+});
+
+test('test character Daemon', () => {
+    const received = new Daemon('Daemon', 'Daemon')
+    const expected = {
+        name: 'Daemon',
+        type: 'Daemon',
+        health: 100,
+        level: 1,
+        attack: 10,
+        defence: 40
+    }
+    expect(received).toEqual(expected);
+});
+
+test('test character Undead', () => {
+    const received = new Undead('Undead', 'Undead')
+    const expected = {
+        name: 'Undead',
+        type: 'Undead',
+        health: 100,
+        level: 1,
+        attack: 25,
+        defence: 25
+    }
+    expect(received).toEqual(expected);
+});
+
+test('test character Zombie', () => {
+    const received = new Zombie('Zombie', 'Zombie')
+    const expected = {
+        name: 'Zombie',
+        type: 'Zombie',
+        health: 100,
+        level: 1,
+        attack: 40,
+        defence: 10
+    }
+    expect(received).toEqual(expected);
+});
